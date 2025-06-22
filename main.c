114de740818f391f0d714c010217f2e09555222c
Marat Saif <maratsaifutdinov22@gmail.com>
2025-06-22 15:24:35 +0500

    Add simple tests for list

--- main.c
+++ main.c
@@ -1,3 +1,4 @@
+#include <assert.h>
 #include <stdio.h>
 #include "linked_list.h"
 #include<unistd.h>
@@ -8,40 +9,32 @@
 #include "utils.h"
 
 void play_with_list();
+void test_list();
 
 int main(void) {
-    // printf("%d\n", 10);
-    // printf("%d\n", 10/10);
-    // printf("%d\n", 10/10/10);
-    play_with_list();
+    test_list();
     return 0;
 }
 
-bool sort_func_(const int a, const int b) {
-    // printf("%d - %d, %d - %d\n", a, a %10, b, b %10);
-    return a % 10 > b % 10;
-}
 
 void play_with_list() {
-    List *list = create_list();
-
-    debug_print_list(list);
-    append_val(list, 1);
-    debug_print_list(list);
-    append_val(list, 22);
-    append_val(list, 41);
-
-    printf("BEFORE INSERTION: \n");
-    debug_print_list(list);
-    insert_val_in_list(&list, 1, 10);
-    printf("AFTER INSERTION: \n");
-    debug_print_list(list);
-
-    printf("\n\n");
-
-    printf("AFTER SORTING:\n");
-    sort_list(list, sort_func_);
-    debug_print_list(list);
+    // debug_print_list(list);
+    // append_val(list, 1);
+    // debug_print_list(list);
+    // append_val(list, 22);
+    // append_val(list, 41);
+    //
+    // printf("BEFORE INSERTION: \n");
+    // debug_print_list(list);
+    // insert_val_in_list(&list, 1, 10);
+    // printf("AFTER INSERTION: \n");
+    // debug_print_list(list);
+    //
+    // printf("\n\n");
+    //
+    // printf("AFTER SORTING:\n");
+    // sort_list(list, sort_func_);
+    // debug_print_list(list);
 
     // print_list(list);
     // printf("BEFORE DELETION:\n");
@@ -51,6 +44,89 @@ void play_with_list() {
     // printf("AFTER DELETION:\n");
     // debug_print_list(list);
     // printf("DELETED VALUE - %d\n", deleted);
+
+}
+
+bool from_small_to_big(const int a, const int b) {
+    return a > b;
+}
+
+void test_list() {
+    printf("TEST LISTS\n");
+
+    List *list = create_list();
+
+    assert(get_len(list) == 0);
+
+    {  // APPEND ONE VAL
+        append_val(list, 10);
+        assert(get_item(list, 0) == 10);
+        assert(get_len(list) == 1);
+    }
+    printf("    APPEND ONE VAL ... OK\n");
+
+
+    {  // APPEND SEVERAL VALS
+        append_vals(list, 3, 100, 300, 400);
+        assert(get_item(list, 1) == 100);
+        assert(get_item(list, 2) == 300);
+        assert(get_item(list, 3) == 400);
+        assert(get_len(list) == 4);
+    }
+    printf("    APPEND SEVERAL VALS ... OK\n");
+
+    {  // INSERT VAL
+        insert_val_in_list(&list, 2, 200);
+        assert(get_item(list, 2) == 200);
+        assert(get_item(list, 1) == 100);
+        assert(get_item(list, 3) == 300);
+        assert(get_len(list) == 5);
+    }
+    printf("    INSERT VAL ... OK\n");
+
+    {  // POP FIRST VAL
+        const int popped_item = pop_item(&list, 0);
+        assert(popped_item == 10);
+        assert(get_len(list) == 4);
+        assert(get_item(list, 0) == 100);
+    }
+    printf("    POP FIRST VAL ... OK\n");
+
+    {  // POP VAL FROM MIDDLE
+        const int popped_item = pop_item(&list, 2);
+        assert(popped_item == 300);
+        assert(get_len(list) == 3);
+        assert(get_item(list, 2) == 400);
+    }
+    printf("    POP VAL FROM MIDDLE ... OK\n");
+
+    {  // POP LAST VAL
+        const int popped_item = pop_item(&list, 2);
+        assert(popped_item == 400);
+        assert(get_len(list) == 2);
+        assert(get_item(list, 1) == 200);
+        assert(get_item(list, 0) == 100);
+    }
+    printf("    POP LAST VAL ... OK\n");
+
+    {  // CLEAR LIST
+        while (get_len(list)) {
+            pop_item(&list, 0);
+        }
+        assert(get_len(list) == 0);
+    }
+
+    {  // SORT LIST
+        append_vals(list, 10, 1000, 1, 345, 7, 3, 1, 6, 54, 9, 10);
+        sort_list(list, from_small_to_big);
+        for (int i = 1; i < get_len(list); i++) {
+            const int prev_item = get_item(list, i-1);
+            const int cur_item = get_item(list, i);
+
+            assert(prev_item <= cur_item);
+        }
+    }
+    printf("    SORT LIST ... OK\n");
 
     delete_list(list);
 }
