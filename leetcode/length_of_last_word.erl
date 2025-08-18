02c3205e5925889de8de7402a68cfd3e80c7dff2
Marat (debian home) <maratsaifutdinov22@gmail.com>
2025-08-18 05:20:21 +0500

    Erlang Easy: length of last word (https://leetcode.com/problems/length-of-last-word/)

---
+++ leetcode/length_of_last_word.erl
@@ -0,0 +1,7 @@
+-module(length_of_last_word).
+
+-export([length_of_last_word/1]).
+
+-spec length_of_last_word(S :: unicode:unicode_binary()) -> integer().
+length_of_last_word(S) ->
+  length(lists:last(string:tokens(binary_to_list(S), " "))).
