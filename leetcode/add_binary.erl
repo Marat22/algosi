b3fa059db528e97702ed250aa49c9b705c5f9c06
Marat (debian home) <maratsaifutdinov22@gmail.com>
2025-08-20 22:36:33 +0500

    Small syntax sugar improvements in `add_binary.erl`

--- leetcode/add_binary.erl
+++ leetcode/add_binary.erl
@@ -1,3 +1,4 @@
+%% https://leetcode.com/problems/add-binary/
 -module(add_binary).
 
 -compile(export_all).
@@ -5,24 +6,19 @@
 -spec add_binary(A :: unicode:unicode_binary(), B :: unicode:unicode_binary()) -> unicode:unicode_binary().
 add_binary(A, B) ->
   list_to_binary(
-        % lists:reverse(
             inner(lists:reverse(binary_to_list(A)), lists:reverse(binary_to_list(B)), "", 0)
-    % )
 ). 
 
-% inner([], [H2 | T2], Res, Add) ->
-%     % NextAdd = case,
-%     inner([], T2, [H2 | Res], 0);
 inner([], [], Res, Add) ->
     case Add of
-        1 -> ["1" | Res];
+        1 -> [$1 | Res];
         0 -> Res
     end;
 inner([H1 | T1], [], Res, Add) ->
     % io:format("~p", [H1]), ""%  ,
     Sum = get_num(H1) + Add,
     case Sum of
-        2 -> inner(T1, [], ["0" | Res], 1);
+        2 -> inner(T1, [], [$0 | Res], 1);
         _ -> inner(T1, [], [get_char(Sum) | Res], 0)
     end;
 inner([], [H2 | T2], Res, Add) ->
@@ -37,17 +33,14 @@ inner([H1 | T1], [H2 | T2], Res, Add) ->
         3 -> inner(T1, T2, ["1" | Res], 1);
         2 -> inner(T1, T2, ["0" | Res], 1);
         _ -> inner(T1, T2, [get_char(Sum) | Res], 0)
-    end;
-inner(A, B, C, D) ->
-    {A, B, C, D}
-.
+    end.
 
-get_num(49) ->
+get_num($1) ->
     1;
-get_num(48) ->
+get_num($0) ->
     0.
 
 get_char(1) ->
-    "1";
+    $1;
 get_char(0) ->
-    "0".
+    $0.
