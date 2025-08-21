7d6d30cb0145930240a2f6837cbd87f1f93beca8
Marat (debian home) <maratsaifutdinov22@gmail.com>
2025-08-22 03:01:15 +0500

    Erlang Easy: Longest Harmonious Subsequence (https://leetcode.com/problems/longest-harmonious-subsequence/description/)

---
+++ leetcode/longest_harmonious_subsequence.erl
@@ -0,0 +1,31 @@
+-module(longest_harmonious_subsequence).
+
+-compile(export_all).
+-spec find_lhs(Nums :: [integer()]) -> integer().
+find_lhs(Nums) ->
+  inner(Nums, #{}, 0).
+
+
+  inner([], _, MaxQty) -> MaxQty;
+inner([H | T], ValsInfo = #{}, MaxQty) ->
+    {NewValsInfo, NewMaxQty} = update_vals_info(H, MaxQty, ValsInfo, true),
+    {NewValsInfo2, NewMaxQty2} = update_vals_info(H+1, NewMaxQty, NewValsInfo, false),
+    inner(T, NewValsInfo2, NewMaxQty2).
+
+
+update_vals_info(Inx, MaxQty, ValsInfo = #{}, IsFirst) ->
+    case {IsFirst, ValsInfo} of
+        {true, #{Inx := {Qty, _, Second = true}}} when MaxQty < Qty + 1 ->
+            {ValsInfo#{Inx => {Qty + 1, true, Second}}, Qty + 1};
+        {true, #{Inx := {Qty, _, Second}}} ->
+            {ValsInfo#{Inx => {Qty + 1, true, Second}}, MaxQty};
+        {true, _} ->
+            {ValsInfo#{Inx => {1, true, false}}, MaxQty};
+
+        {false, #{Inx := {Qty, First=true, _}}} when MaxQty < Qty + 1 ->
+            {ValsInfo#{Inx => {Qty + 1, First, true}}, Qty + 1};
+        {false, #{Inx := {Qty, First, _}}} ->
+            {ValsInfo#{Inx => {Qty + 1, First, true}}, MaxQty};
+        {false, _} ->
+            {ValsInfo#{Inx => {1, false, true}}, MaxQty}
+    end.
