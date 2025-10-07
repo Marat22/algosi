328e8403816b9ed8febfe3a11ac7ae3f6d126fe4
Marat (kali home) <maratsaifutdinov22@gmail.com>
2025-10-07 23:28:25 +0500

    Python Easy Yandex: Ball (https://contest.yandex.ru/contest/80940/problems/H/)

---
+++ yandex_trainings/2_A_ball.py
@@ -0,0 +1,14 @@
+N = int(input())
+
+steps = [0] * max(2+N, 4)
+
+steps[0] = 0
+steps[1] = 1
+steps[2] = steps[2-1] + 1
+steps[3] = steps[3-1] + steps[3-2] + 1
+
+for i in range(4, N+1):
+    steps[i] = steps[i-3] + steps[i-2] + steps[i-1]
+
+print(steps)
+print(steps[N])
