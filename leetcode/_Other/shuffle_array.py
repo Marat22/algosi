baea00a8d50a95cf8e17b141b75d8991e129e2bf
Marat (kali home) <maratsaifutdinov22@gmail.com>
2025-12-22 02:20:09 +0500

    Python Easy: Shuffle the Array (https://leetcode.com/problems/shuffle-the-array/description/)

---
+++ leetcode/_Other/shuffle_array.py
@@ -0,0 +1,9 @@
+# https://leetcode.com/problems/shuffle-the-array/description/
+
+class Solution:
+    def shuffle(self, nums: List[int], n: int) -> List[int]:
+        res = []
+        for x, y in zip( nums[:n], nums[n:] ):
+            res.append(x)
+            res.append(y)
+        return res
